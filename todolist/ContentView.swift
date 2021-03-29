d215023a13ebe3416c724380c1b62947cfbe7dff
joferava <jfelipe221095@gmail.com>
2021-03-29 17:54:48 -0600

    Initial Commit

---
+++ todolist/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  todolist
+//
+//  Created by Felipe Ramirez Vargas on 29/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ todolist/todolistApp.swift
@@ -0,0 +1,17 @@
+//
+//  todolistApp.swift
+//  todolist
+//
+//  Created by Felipe Ramirez Vargas on 29/3/21.
+//
+
+import SwiftUI
+
+@main
+struct todolistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
